7e06da5da86a5b984deb5f94b817f412e525aa3f
m.yektamaram <mohammad.1ta@gmail.com>
2019-09-26 10:31:24 +0330

    Added socket.io connection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
+});
+
+app.use( express.static( __dirname + '/public' ) );
+
+io.on('connection', function(socket) {
+
 });
