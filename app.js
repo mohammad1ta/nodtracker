54c2913038918d4e2a695cacfc7045cff4e459b5
m.yektamaram <mohammad.1ta@gmail.com>
2019-09-27 19:20:52 +0330

    Counter for online users

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var onlineUsers = 0;
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
 app.use( express.static( __dirname + '/public' ) );
 
 io.on('connection', function(socket) {
+
+    onlineUsers++;
+
+    socket.on('disconnect', function() {
+        onlineUsers--;
+    });
 
 });
